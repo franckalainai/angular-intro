51e71b2aa665867ded68a19eb85f47356e483daa
franckalainai <franckalain.ai@gmail.com>
2021-08-13 15:23:09 +0000

    data binding - communication between component class and html view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
