a1dc15fda4eacf57ec457f29f10e50b23d9c4585
franckalainai <franckalain.ai@gmail.com>
2021-08-13 17:54:17 +0000

    classes et styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   name = 'Franck';
   counter = null;
+
+  myColor = 'gray';
+
   people = [
     {name: 'franck', age: 37},
     {name: 'martial', age: 40},
@@ -18,6 +21,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     this.counter = 0;
+    this.myColor = 'red'
   }
 
   btnClicked = () => {
